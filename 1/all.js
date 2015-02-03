99a06fac554f3c6339a9a9c666d2b302701ba6fc
helloiamvu <vuhcze@gmail.com>
2015-02-03 22:01:35 +0100

    Adroll script deleted and GA script added

--- 1/all.js
+++ 1/all.js
@@ -49,18 +49,10 @@ window.onload = function() {
 })();
 
 
-adroll_adv_id = "EN2QNXTGSNBBLKA4A6YGFV";
-adroll_pix_id = "Z6Q5RNRMTRCVVAEW722SNF";
-(function () {
-var oldonload = window.onload;
-window.onload = function(){
-   __adroll_loaded=true;
-   var scr = document.createElement("script");
-   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-   scr.setAttribute('async', 'true');
-   scr.type = "text/javascript";
-   scr.src = host + "/j/roundtrip.js";
-   ((document.getElementsByTagName('head') || [null])[0] ||
-    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-   if(oldonload) {oldonload()}};
-}());
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-46020418-14', 'auto');
+ga('send', 'pageview');

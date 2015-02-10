abc9640d4fc7825b34f7ea7d16d5918e090b792c
patrikholcak <patrik@holcak.com>
2015-02-10 22:42:13 +0100

    update ga tracker

--- 1/all.js
+++ 1/all.js
@@ -54,5 +54,5 @@ window.onload = function() {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-46020418-14', 'auto');
-ga('send', 'pageview');
+ga('create', 'UA-46020418-14', 'auto', {'name': 'sourceTracker'});
+ga('sourceTracker.send', 'pageview');
